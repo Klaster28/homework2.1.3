abce898c73210c1ebbed117c38b2c06497fa6f97
Klaster28 <89096914521@mail.ru>
2022-11-23 10:21:04 +0300

    Коммит после дополнения 3го задания

--- src/Car.java
+++ src/Car.java
@@ -1,19 +1,48 @@
 public class Car {
-    String brand;
-    String model;
-    double engineVolume;
-    String color;
-    int productionYear;
-    String productionCountry;
+    private String brand;
+    private String model;
+    private double engineVolume;
+    private String color;
+   private int productionYear;
+    private String productionCountry;
 
 
     Car(String brand, String model, double engineVolume, String color, int productionYear, String productionCountry) {
-        this.brand = brand;
-        this.model = model;
-        this.engineVolume = engineVolume;
-        this.color = color;
-        this.productionYear = productionYear;
-        this.productionCountry = productionCountry;
+       if (brand == null || brand.isEmpty() || brand.isBlank()){
+           this.brand = "default";
+       }else {
+           this.brand = brand;
+       }
+
+        if (model == null || brand.isEmpty() || brand.isBlank()){
+            this.model = "default";
+        }else {
+            this.model = model;
+        }
+
+        if (engineVolume < 0){
+            this.engineVolume = 1.5;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+
+        if (color == null || brand.isEmpty() || brand.isBlank()){
+            this.color = "белый";
+        }else {
+            this.color = color;
+        }
+
+        if (productionYear < 0) {
+            this.productionYear = 2000;
+        } else {
+            this.productionYear = productionYear;
+        }
+
+        if (productionCountry == null || productionCountry.isEmpty() || productionCountry.isBlank()) {
+            this.productionCountry = "default";
+        } else {
+            this.productionCountry = productionCountry;
+        }
     }
 
     public String getBrand() {
